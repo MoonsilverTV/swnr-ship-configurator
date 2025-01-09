003249ab1f951f4f5f05906c24b6aebf5323218f
Stefan Erwin Baumer <baumeredv@gmail.com>
2025-01-09 20:26:18 +0100

    display ship hull table

--- src/main/moontv/swnr/shipconfigurator/app.cljs
+++ src/main/moontv/swnr/shipconfigurator/app.cljs
@@ -24,8 +24,26 @@
 (rf/reg-event-db              ;; sets up initial application state
  :initialize                 ;; usage:  (dispatch [:initialize])
  (fn [_ _]                   ;; the two parameters are not important here, so use _
-   {:time (js/Date.)         ;; What it returns becomes the new application state
-    :time-color "orange"}))  ;; so the application state will initially be a map with two keys
+   (let [ship-data (->> {:strike-fighter ["Strike Fighter" 200000 5 5 8 1 1 16 5 2 1 :fighter]
+                         :shuttle ["Shuttle" 200000 3 0 15 1 10 11 3 5 1 :fighter]
+                         :free-merchant ["Free Merchant" 500000 3 2 20 1 6 14 10 15 2 :frigate]
+                         :patrol-boat ["Patrol Boat" 2500000 4 5 25 5 20 14 15 10 4 :frigate]
+                         :corvette ["Corvette" 4000000 2 10 40 10 40 13 15 15 6 :frigate]
+                         :heavy-frigate ["Heavy Frigate" 7000000 1 10 50 30 120 15 25 20 8 :frigate]
+                         :bulk-freighter ["Bulk Freighter" 5000000 0 0 40 10 40 11 15 25 2 :cruiser]
+                         :fleet-cruiser ["Fleet Cruiser" 10000000 1 15 60 50 200 14 50 30 10 :cruiser]
+                         :battleship ["Battleship" 50000000 0 20 100 200 1000 16 75 50 15 :capital]
+                         :small-station ["Small Station" 5000000 nil 5 120 20 200 11 50 40 10 :cruiser]
+                         :large-station ["Large Station" 40000000 nil 20 120 100 1000 17 125 75 30 :capital]}
+                        seq
+                        (map (fn [[key value]]
+                               [key
+                                (zipmap [:name :cost :speed :armor :hp :crew-min :crew-max :ac :power :mass :hardpoints :class]
+                                        value)]))
+                        (into (sorted-map)))]
+     {:time (js/Date.)         ;; What it returns becomes the new application state
+      :time-color "orange"
+      :ship-data ship-data})))  ;; so the application state will initially be a map with two keys
 
 (rf/reg-event-db                ;; usage:  (dispatch [:time-color-change 34562])
  :time-color-change            ;; dispatched when the user enters a new colour into the UI text field
@@ -49,6 +67,11 @@
  (fn [db _]
    (:time-color db)))
 
+(rf/reg-sub
+ :ship-data
+ (fn [db _]
+   (:ship-data db)))
+
 ;; -- Domino 5 - View Functions ----------------------------------------------
 
 (defn clock
@@ -58,7 +81,7 @@
                    .toTimeString
                    (clojure.string/split " ")
                    first)]
-    [:div.example-clock {:style {:color colour}} time]))
+    [:div.example-clock {:style {:color colour :font-size "48px"}} time]))
 
 (defn color-input
   []
@@ -71,12 +94,63 @@
               :value @(rf/subscribe [:time-color])        ;; subscribe
               :on-change emit}]]))  ;; <---
 
+(defn hull-selector
+  []
+  (let [ships @(rf/subscribe [:ship-data])]
+    [:details
+     {:open true}
+     [:table
+      [:thead [:tr (->> '("Hull Type" "Cost" "Speed" "Armor" "HP" "Crew-Min" "Crew-Max" "AC" "Power" "Mass" "Hardpoints" "Class")
+                        (map (fn [column-name] [:th {:key column-name} column-name])))]] ;;todo use index instead of column name for key
+      [:tbody
+       (->> ships
+            seq
+            (map (fn [[type data]]
+                   [:tr {:key type}
+                    [:td (:name data)] ;;REVIEW: is there a better way to do this?
+                    [:td (:cost data)]
+                    [:td (:speed data)]
+                    [:td (:armor data)]
+                    [:td (:hp data)]
+                    [:td (:crew-min data)]
+                    [:td (:crew-max data)]
+                    [:td (:ac data)]
+                    [:td (:power data)]
+                    [:td (:mass data)]
+                    [:td (:hardpoints data)]
+                    [:td (:class data)] ;;TODO: convert to readable string
+                    ])))]]]))
+(defn fitting-selector
+  []
+  [:div "this is the fitting selector"])
+
+(defn defenses-selector
+  []
+  [:div "this is the defenses selector"])
+
+(defn weaponry-selector
+  []
+  [:div "this is the weaponry selector"])
+
+(defn starship-summary
+  []
+  [:div "this is the starship summary"])
+
+(defn swnr-starship-configurator-app
+  []
+  [:div
+   [:h1 "SWNr Starship Configurator"]
+   [hull-selector]
+   [fitting-selector]
+   [defenses-selector]
+   [weaponry-selector]
+   [starship-summary]])
+
 (defn ui
   []
   [:div
-   [:h1 "The time is now:"]
-   [clock]
-   [color-input]])
+   [:h1 "ze website"]
+   [swnr-starship-configurator-app]])
 
 ;; -- Entry Point -------------------------------------------------------------
 
