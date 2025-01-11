a27567779278adf36a3a78e26e3e4fbb5d20c756
Stefan Erwin Baumer <baumeredv@gmail.com>
2025-01-11 13:38:35 +0100

    remove timer code from original reframe example

--- src/main/moontv/swnr/shipconfigurator/app.cljs
+++ src/main/moontv/swnr/shipconfigurator/app.cljs
@@ -12,15 +12,15 @@
          @db/app-db)
 ;; -- Domino 1 - Event Dispatch -----------------------------------------------
 
-(defn dispatch-timer-event
-  []
-  (let [now (js/Date.)]
-    (rf/dispatch [:timer now])))  ;; <-- dispatch used
+;;(defn dispatch-timer-event
+;;  []
+;;  (let [now (js/Date.)]
+;;    (rf/dispatch [:timer now])))  ;; <-- dispatch used
 
 ;; Call the dispatching function every second.
 ;; `defonce` is like `def` but it ensures only one instance is ever
 ;; created in the face of figwheel hot-reloading of this file.
-(defonce do-timer (js/setInterval dispatch-timer-event 1000))
+;(defonce do-timer (js/setInterval dispatch-timer-event 1000))
 
 ;; -- Domino 2 - Event Handlers -----------------------------------------------
 
@@ -43,20 +43,8 @@
                                 (zipmap [:name :cost :speed :armor :hp :crew-min :crew-max :ac :power :mass :hardpoints :class]
                                         value)]))
                         (into {}))] ;; TODO: fix ordering in UI (maybe i don't even want a map and instead want a [[] [] []...])
-     {:time (js/Date.)         ;; What it returns becomes the new application state
-      :time-color "orange"
-      :ship-data ship-data
+     {:ship-data ship-data
       :selected-ship :large-station})))  ;; so the application state will initially be a map with two keys
-
-(rf/reg-event-db                ;; usage:  (dispatch [:time-color-change 34562])
- :time-color-change            ;; dispatched when the user enters a new colour into the UI text field
- (fn [db [_ new-color-value]]  ;; -db event handlers given 2 parameters:  current application state and event (a vector)
-   (assoc db :time-color new-color-value)))   ;; compute and return the new application state
-
-(rf/reg-event-db                 ;; usage:  (dispatch [:timer a-js-Date])
- :timer                         ;; every second an event of this kind will be dispatched
- (fn [db [_ new-time]]          ;; note how the 2nd parameter is destructured to obtain the data value
-   (assoc db :time new-time)))  ;; compute and return the new application state
 
 (rf/reg-event-db
  :select-ship
@@ -66,16 +54,6 @@
        (assoc db :selected-ship type)))))
 
 ;; -- Domino 4 - Query  -------------------------------------------------------
-
-(rf/reg-sub
- :time
- (fn [db _]     ;; db is current app state. 2nd unused param is query vector
-   (:time db))) ;; return a query computation over the application state
-
-(rf/reg-sub
- :time-color
- (fn [db _]
-   (:time-color db)))
 
 (rf/reg-sub
  :ship-data
@@ -88,26 +66,6 @@
    (:selected-ship db)))
 
 ;; -- Domino 5 - View Functions ----------------------------------------------
-
-(defn clock
-  []
-  (let [colour @(rf/subscribe [:time-color])
-        time   (-> @(rf/subscribe [:time])
-                   .toTimeString
-                   (clojure.string/split " ")
-                   first)]
-    [:div.example-clock {:style {:color colour :font-size "48px"}} time]))
-
-(defn color-input
-  []
-  (let [gettext (fn [e] (-> e .-target .-value))
-        emit    (fn [e] (rf/dispatch [:time-color-change (gettext e)]))]
-    [:div.color-input
-     "Display color: "
-     [:input {:type "text"
-              :style {:border "1px solid #CCC"}
-              :value @(rf/subscribe [:time-color])        ;; subscribe
-              :on-change emit}]]))  ;; <---
 
 (defn hull-selector-line [[ship-type ship-data] on-mouse-down]
   ^{:key ship-type}
