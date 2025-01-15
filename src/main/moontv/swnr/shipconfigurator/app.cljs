5a551188e57dadfaa619bc4792f4c575570da36d
Stefan Erwin Baumer <baumeredv@gmail.com>
2025-01-15 20:01:46 +0100

    started a db spec

--- src/main/moontv/swnr/shipconfigurator/app.cljs
+++ src/main/moontv/swnr/shipconfigurator/app.cljs
@@ -2,6 +2,9 @@
   (:require
    [clojure.string]
    [re-frame.core :as rf]
+   ;; TODO import events file
+   ;; TODO import subs file
+   [moontv.swnr.shipconfigurator.db-spec :as db-s]
    [reagent.dom.client :as rdc]))
 
 ;; A detailed walk-through of this source code is provided in the docs:
@@ -27,30 +30,33 @@
 (rf/reg-event-db              ;; sets up initial application state
  :initialize                 ;; usage:  (dispatch [:initialize])
  (fn [_ _]                   ;; the two parameters are not important here, so use _
-   (let [ship-data (->> {:strike-fighter ["Strike Fighter" 200000 5 5 8 1 1 16 5 2 1 :fighter]
-                         :shuttle ["Shuttle" 200000 3 0 15 1 10 11 3 5 1 :fighter]
-                         :free-merchant ["Free Merchant" 500000 3 2 20 1 6 14 10 15 2 :frigate]
-                         :patrol-boat ["Patrol Boat" 2500000 4 5 25 5 20 14 15 10 4 :frigate]
-                         :corvette ["Corvette" 4000000 2 10 40 10 40 13 15 15 6 :frigate]
-                         :heavy-frigate ["Heavy Frigate" 7000000 1 10 50 30 120 15 25 20 8 :frigate]
-                         :bulk-freighter ["Bulk Freighter" 5000000 0 0 40 10 40 11 15 25 2 :cruiser]
-                         :fleet-cruiser ["Fleet Cruiser" 10000000 1 15 60 50 200 14 50 30 10 :cruiser]
-                         :battleship ["Battleship" 50000000 0 20 100 200 1000 16 75 50 15 :capital]
-                         :small-station ["Small Station" 5000000 nil 5 120 20 200 11 50 40 10 :cruiser]
-                         :large-station ["Large Station" 40000000 nil 20 120 100 1000 17 125 75 30 :capital]}
+   (let [ship-data (->> {:strike-fighter ["Strike Fighter" 200000 5 5 8 1 1 16 5 2 1 ::db-s/fighter]
+                         :shuttle ["Shuttle" 200000 3 0 15 1 10 11 3 5 1 ::db-s/fighter]
+                         :free-merchant ["Free Merchant" 500000 3 2 20 1 6 14 10 15 2 ::db-s/frigate]
+                         :patrol-boat ["Patrol Boat" 2500000 4 5 25 5 20 14 15 10 4 ::db-s/frigate]
+                         :corvette ["Corvette" 4000000 2 10 40 10 40 13 15 15 6 ::db-s/frigate]
+                         :heavy-frigate ["Heavy Frigate" 7000000 1 10 50 30 120 15 25 20 8 ::db-s/frigate]
+                         :bulk-freighter ["Bulk Freighter" 5000000 0 0 40 10 40 11 15 25 2 ::db-s/cruiser]
+                         :fleet-cruiser ["Fleet Cruiser" 10000000 1 15 60 50 200 14 50 30 10 ::db-s/cruiser]
+                         :battleship ["Battleship" 50000000 0 20 100 200 1000 16 75 50 15 ::db-s/capital]
+                         :small-station ["Small Station" 5000000 nil 5 120 20 200 11 50 40 10 ::db-s/cruiser]
+                         :large-station ["Large Station" 40000000 nil 20 120 100 1000 17 125 75 30 ::db-s/capital]}
                         (map (fn [[key value]]
                                [key
-                                (zipmap [:name :cost :speed :armor :hp :crew-min :crew-max :ac :power :mass :hardpoints :class]
+                                (zipmap [::db-s/ship-name ::db-s/ship-cost ::db-s/ship-speed ::db-s/ship-armor
+                                         ::db-s/ship-hp ::db-s/ship-crew-min ::db-s/ship-crew-max ::db-s/ship-ac
+                                         ::db-s/ship-power ::db-s/ship-mass ::db-s/ship-hardpoints ::db-s/ship-class]
                                         value)]))
                         (into {}))] ;; TODO: fix ordering in UI (maybe i don't even want a map and instead want a [[] [] []...])
-     {:ship-data ship-data
-      :selected-ship :large-station})))  ;; so the application state will initially be a map with two keys
+     {::db-s/ship-data ship-data
+      ::db-s/selected-ship :large-station})))  ;; so the application state will initially be a map with two keys
 
 (rf/reg-event-db
  :select-ship
  (fn [db [_event-name type]]
-   (when (not (identical? (:selected-ship db) type))
+   (when (not (= (:selected-ship db) type))
      (when (js/confirm "Changing your hull type will delete your data, are you sure?") ;; REVIEW: is this impurity okay?
+       ;; TODO: implement undo / redo so we no longer need that check
        (assoc db :selected-ship type)))))
 
 ;; -- Domino 4 - Query  -------------------------------------------------------
---
+++ src/main/moontv/swnr/shipconfigurator/db_spec.cljs
@@ -0,0 +1,37 @@
+(ns moontv.swnr.shipconfigurator.db-spec
+  (:require [clojure.spec.alpha :as s]
+            [re-frame.db :as db]))
+
+(def pos-or-zero (s/or ::positive pos? ::zero zero?))
+
+(s/def ::ship-id keyword?) ;; TODO constrain this or no? ...
+(s/def ::selected-ship keyword?);; FIXME ::ship-id)
+(s/def ::ship-name (s/and string? not-empty))
+(s/def ::ship-cost (s/and number? pos-or-zero))
+(s/def ::ship-speed (s/nilable (s/and int? pos-or-zero)))
+(s/def ::ship-armor (s/int-in 0 51))
+(s/def ::ship-hp (s/int-in 0 1000))
+(s/def ::ship-crew-min (s/int-in 1 10000))
+(s/def ::ship-crew-max (s/int-in 1 10000))
+(s/def ::ship-ac (s/int-in 1 36))
+(s/def ::ship-power (s/int-in 1 1000))
+(s/def ::ship-mass (s/int-in 1 1000))
+(s/def ::ship-hardpoints (s/int-in 1 100))
+(s/def ::ship-class #{::fighter ::frigate ::cruiser ::capital})
+
+(s/def ::ship-data-record (s/and
+                           (s/keys :req [::ship-name ::ship-cost ::ship-speed
+                                         ::ship-armor ::ship-hp ::ship-crew-min
+                                         ::ship-crew-max ::ship-ac ::ship-power
+                                         ::ship-mass ::ship-hardpoints ::ship-class])
+                           #(<= (::ship-crew-min %) (::ship-crew-max %))))
+
+(s/def ::ship-data (s/coll-of (s/tuple ::ship-id ::ship-data-record) :distinct true)) ;; TODO distinct id keys
+
+(s/def ::app-db (s/keys :req [::ship-data ::selected-ship]))
+
+(comment
+  #_{:clj-kondo/ignore [:unresolved-namespace]}
+  (re-frame.core/dispatch :initialize)
+  (s/explain ::app-db @re-frame.db/app-db))
+
